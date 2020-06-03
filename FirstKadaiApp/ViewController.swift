b0bc1409e49376c421d8358919929774793ec94c
70moogie <70moogie@gmail.com>
2020-06-03 09:48:18 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by St_NY on 2020/06/03.
+//  Copyright © 2020 70moogie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
